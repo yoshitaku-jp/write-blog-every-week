0b060d2e8b8dbef7bf1e5f7774a9d473e4b34091
Takuya YOSHIMURA <yoshitaku-jp@users.noreply.github.com>
2019-01-08 18:16:16 +0900

    import Vue from 'vue';

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,15 @@ import PageHeader from './components/PageHeader.vue';
 import PageFooter from './components/PageFooter.vue';
 import Posts from './Posts.vue';
 
+import Vue from 'vue';
+window.Vue = Vue;
+
 import VueGtm from 'vue-gtm';
 
-//Vue.use(VueGtm, {
-//    id: 'GTM-W6DPH4H', // Your GTM ID
-//    enabled: true, // defaults to true. Plugin can be disabled by setting this to false for Ex: enabled: !!GDPR_Cookie (optional)
-//  });
+Vue.use(VueGtm, {
+    id: 'GTM-W6DPH4H', 
+    enabled: true
+  });
 
 
 module.exports = {
