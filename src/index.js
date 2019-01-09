f494d91805d02c8435db341a7d10f087efdeb388
Takuya YOSHIMURA <yoshitaku-jp@users.noreply.github.com>
2019-01-09 11:43:59 +0900

    Initialize Firebaseの設定をApp.vueに移動

--- src/index.js
+++ src/index.js
@@ -1,19 +1,5 @@
 import Vue from 'vue';
 import App from './App.vue';
-import firebase from 'firebase';
-
-
-// Initialize Firebase
-var config = {
-    apiKey: process.env.NODE_API_KEY,
-    authDomain: process.env.NODE_AUTH_DOMAIN,
-    databaseURL: process.env.NODE_DATABASE_URL,
-    projectId: process.env.NODE_PROJECT_ID,
-    storageBucket: process.env.NODE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NODE_MESSAGING_SENDER_ID
-}
-
-firebase.initializeApp(config)
 
 new Vue({
     el: '#app',
