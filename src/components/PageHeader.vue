c3e2783498a4b0d2a8811e0b3b2aea906c88ca80
Takuya YOSHIMURA <yoshitaku-jp@users.noreply.github.com>
2019-02-19 18:31:34 +0900

    ヘッダーにルールを記載

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,5 +1,11 @@
 <template>
-  <nav class="page_header"><h1>Write Blog Every Week</h1></nav>
+  <nav class="page_header"><h1>Write Blog Every Week</h1>
+  <ul>
+    <li><h2>1. 週に1記事更新すること</h2></li>
+    <li><h2>2. 更新できなかったら翌週2記事更新すること</h2></li>
+    <li><h2>3. 3記事更新が滞った場合退会となる</h2></li>
+  </ul>
+  </nav>
 </template>
 
 <script>
@@ -19,5 +25,10 @@ h1 {
   text-align: center;
   font-size: 64px;
   color: rgb(0, 0, 0);
+}
+
+ul li{
+  text-align: left;
+  list-style: none;
 }
 </style>
