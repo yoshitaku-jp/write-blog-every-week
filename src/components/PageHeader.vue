12fa440286be09e6570b7390a12080f73e13a0e7
Takuya YOSHIMURA <yoshitaku-jp@users.noreply.github.com>
2019-06-06 16:44:42 +0900

    headerの調整

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,24 +1,44 @@
 <template>
-  <nav class="page_header"><h1>Write Blog Every Week</h1></nav>
+  <nav class="page_header">
+    <h1>Write Blog Every Week</h1>
+  </nav>
 </template>
 
 <script>
 export default {
-  name: 'page-header'
+  name: "page-header"
 };
 </script>
 
 <style lang="scss" scoped>
 @font-face {
-  font-family: 'IsaaxFont';
-  src: url('~src/assets/fonts/IsaaxOrigin-Regular.woff2');
+  font-family: "IsaaxFont";
+  src: url("~src/assets/fonts/IsaaxOrigin-Regular.woff2");
 }
 
-h1 {
-  font-family: 'IsaaxFont';
-  text-align: center;
+nav {
+  position: fixed;
+  width: 1440px;
+  height: 90px;
+  left: 0px;
+  top: 0px;
+
+  background: #000000;
+}
+
+nav h1 {
+  position: fixed;
+  width: 1440px;
+  height: 90px;
+  margin: 0;
+
+  font-family: "IsaaxFont";
   font-size: 64px;
-  color: rgb(0, 0, 0);
+  line-height: 75px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  color: #ffffff;
 }
 
 ul li {
