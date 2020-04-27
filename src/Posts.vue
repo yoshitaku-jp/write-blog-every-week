3549e3cefa9eead6e9736a770997e106dac7f8b9
TAKUYA Yoshimura <yoshimura.takuya1990@gmail.com>
2020-04-27 10:23:53 +0900

    changed the URL where json is located

--- src/Posts.vue
+++ src/Posts.vue
@@ -3,24 +3,30 @@
     <v-layout wrap justify-space-around>
       <v-flex v-for="blog in sortedBlogs" :key="blog.id">
         <v-card class="grey ma-2 flexcard" width="400px" height="260px">
-
           <v-card-title class="subtitle justify-center">
-            <v-list-tile :href="blog.blogurl" target="_blank" >{{ blog.blogtitle }}</v-list-tile>
+            <v-list-tile :href="blog.blogurl" target="_blank">{{
+              blog.blogtitle
+            }}</v-list-tile>
           </v-card-title>
 
           <v-card-text class="grow headline">
-            <v-list-tile :href="blog.url" target="_blank">{{ blog.title }}</v-list-tile >
+            <v-list-tile :href="blog.url" target="_blank">{{
+              blog.title
+            }}</v-list-tile>
           </v-card-text>
 
           <v-card-actions>
             <span>
-              <v-card-media width="48px" height="48px" :src="blog.icon"></v-card-media>
+              <v-card-media
+                width="48px"
+                height="48px"
+                :src="blog.icon"
+              ></v-card-media>
             </span>
             <span>
               <v-card-text>{{ blog.name }}</v-card-text>
             </span>
           </v-card-actions>
-
         </v-card>
       </v-flex>
     </v-layout>
@@ -40,7 +46,7 @@ export default {
   },
   created: function() {
     var _this = this;
-    const URL = 'https://api-wbew-go.netlify.com/blogs.json';
+    const URL = 'https://api-wbew-go.netlify.app/blogs.json';
 
     axios.get(URL).then(function(response) {
       _this.blogs = response.data;
